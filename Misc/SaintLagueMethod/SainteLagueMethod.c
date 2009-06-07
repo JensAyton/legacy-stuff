de87bd530f32d6db1837a62dd9f011b746b8d5b1
Jens Ayton <github.j@ayton.se>
2009-06-07 01:09:38 +0000

    Checked in SaintLagueMethod
    git-svn-id: https://ahruman.googlecode.com/svn/trunk@37 23014adc-a008-11dd-9a14-c1645b63b0c6

---
+++ Misc/SaintLagueMethod/SainteLagueMethod.c
@@ -0,0 +1,131 @@
+/*
+	SainteLagueMethod.h
+	Jens Ayton, 2009
+	
+	Calculate vote apportionment with the modified Sainte-Lagüe method
+	(using an initial divisor of 1.4) and a threshold, as used in (among
+	others) Swedish elections. See
+	http://en.wikipedia.org/wiki/Sainte-Lagu%C3%AB_method or
+	http://www.val.se/det_svenska_valsystemet/valresultat/rakneexempel/index.html
+	for more information, or the Swedish election law (SFS 2005:837) chapter
+	14 section 3 for the normative (for Sweden) definition.
+	
+	This implementation uses some simple algebraic transformations of the
+	"pure" algorithm to allow it to work entirely with integer mathematics.
+	
+	This code is hereby released under the CC0 license:
+	
+	The person who associated a work with this document has dedicated this
+	work to the Commons by waiving all of his or her rights to the work under
+	copyright law and all related or neighboring legal rights he or she had in
+	the work, to the extent allowable by law.
+	
+		Other Rights — In no way are any of the following rights affected by
+		CC0:
+			* Patent or trademark rights held by the person who associated
+			  this document with a work.
+			* Rights other persons may have either in the work itself or in
+			  how the work is used, such as publicity or privacy rights.
+	
+	http://wiki.creativecommons.org/CC0
+*/
+
+#include "SainteLagueMethod.h"
+#include <assert.h>
+
+
+typedef struct SLCalcInfo
+{
+	void				*tag;			// For client to identify parties.
+	uintmax_t			votes;			// Absolute number of votes cast.
+	uintmax_t			divisor;		// Divisor, multiplied by ten to allow integer maths. Starts at 14.
+	uintmax_t			nextDivisor;	// Next divisor to use (after a seat has been assigned), multiplied by ten. Starts at 30, increases by 20 per seat.
+	uintmax_t			seats;			// Seats assigned.
+} SLCalcInfo;
+
+
+static int CompareSLCalcInfo(const void *a, const void *b)
+{
+	const SLCalcInfo *infoA = a;
+	const SLCalcInfo *infoB = b;
+	
+	/*	Calculate comparators. Note that comparing A.votes * B.divisor with
+		B.votes * A.divisor is algebraically equivalent to comparing
+		A.votes / A.divisor with B.votes / B.divisor, but allows us to stick
+		with integer maths.
+		
+		Also note that this is reverse sort order (biggest result first).
+	*/
+	uintmax_t			compA = infoA->votes * infoB->divisor;
+	uintmax_t			compB = infoB->votes * infoA->divisor;
+	
+	if (compA < compB)  return 1;
+	if (compA > compB)  return -1;
+	return 0;
+}
+
+
+void SLCalculate(SLParty *parties, size_t count, uintmax_t seats, uintmax_t thresholdNumerator, uintmax_t thresholdDenominator)
+{
+	uintmax_t			i, totalVotes = 0;
+	SLParty			excludedParties[14];
+	SLCalcInfo			calcParties[14];
+	size_t				excludedCount = 0, calcCount = 0;
+	
+	assert(parties != NULL);
+	assert(thresholdDenominator != 0 && thresholdNumerator != 0);
+	
+	// Count total votes.
+	for (i = 0; i < count; i++)
+	{
+		totalVotes += parties[i].votes;
+	}
+	
+	// Divide parties into excluded (below threshold) and calc.
+	for (i = 0; i < count; i++)
+	{
+		if (!parties[i].valid || (parties[i].votes * thresholdDenominator < totalVotes * thresholdNumerator))
+		{
+			excludedParties[excludedCount++] = parties[i];
+		}
+		else
+		{
+			calcParties[calcCount].tag = parties[i].tag;
+			calcParties[calcCount].votes = parties[i].votes;
+			calcParties[calcCount].divisor = 14;
+			calcParties[calcCount].nextDivisor = 30;
+			calcParties[calcCount].seats = 0;
+			calcCount++;
+		}
+	}
+	
+	// Assign seats.
+	if (calcCount > 0)
+	{
+		for (i = 0; i < seats; i++)
+		{
+			qsort(calcParties, calcCount, sizeof *calcParties, CompareSLCalcInfo);
+			
+			// Zeroth party in list is next assignee
+			calcParties[0].seats++;
+			calcParties[0].divisor = calcParties[0].nextDivisor;
+			calcParties[0].nextDivisor += 20;
+		}
+	}
+	
+	// Merge calcParties and excludedParties into result.
+	for (i = 0; i < calcCount; i++)
+	{
+		parties[i].tag = calcParties[i].tag;
+		parties[i].votes = calcParties[i].votes;
+		parties[i + calcCount].valid = true;
+		parties[i].seats = calcParties[i].seats;
+	}
+	for (i = 0; i < excludedCount; i++)
+	{
+		parties[i + calcCount].tag = excludedParties[i].tag;
+		parties[i + calcCount].votes = excludedParties[i].votes;
+		parties[i + calcCount].valid = excludedParties[i].valid;
+		parties[i + calcCount].seats = 0;
+	}
+}
---
+++ Misc/SaintLagueMethod/SainteLagueMethod.h
@@ -0,0 +1,53 @@
+/*
+	SainteLagueMethod.h
+	Jens Ayton, 2009
+	
+	Calculate vote apportionment with the modified Sainte-Lagüe method
+	(using an initial divisor of 1.4) and a threshold, as used in (among
+	others) Swedish elections. See
+	http://en.wikipedia.org/wiki/Sainte-Lagu%C3%AB_method or
+	http://www.val.se/det_svenska_valsystemet/valresultat/rakneexempel/index.html
+	for more information, or the Swedish election law (SFS 2005:837) chapter
+	14 section 3 for the normative (for Sweden) definition.
+	
+	This implementation uses some simple algebraic transformations of the
+	"pure" algorithm to allow it to work entirely with integer mathematics.
+	
+	This code is hereby released under the CC0 license:
+	
+	The person who associated a work with this document has dedicated this
+	work to the Commons by waiving all of his or her rights to the work under
+	copyright law and all related or neighboring legal rights he or she had in
+	the work, to the extent allowable by law.
+	
+		Other Rights — In no way are any of the following rights affected by
+		CC0:
+			* Patent or trademark rights held by the person who associated
+			  this document with a work.
+			* Rights other persons may have either in the work itself or in
+			  how the work is used, such as publicity or privacy rights.
+	
+	http://wiki.creativecommons.org/CC0
+*/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+typedef struct SLParty
+{
+	void				*tag;	// For client to identify parties
+	uintmax_t			votes;	// Input
+	bool				valid;	// True (on input) if party can receive seats (set to false for spoiled ballots etc)
+	uintmax_t			seats;	// Output
+} SLParty;
+
+
+/*	Calculate election result using Swedish counting method, “jämkade
+	uddatalsmetoden”. On input, parties is an array of SLParty with tag and
+	votes set. On return, it contains the same parties in arbitrary order with
+	seats filled in; the client code should use the tag fields to identify
+	parties.
+*/
+void SLCalculate(SLParty *parties, size_t count, uintmax_t seats, uintmax_t thresholdNumerator, uintmax_t thresholdDenominator);
